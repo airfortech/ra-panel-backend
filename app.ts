5274b7e13792790e79c5045cbe1c74293b5b6660
airfortech <airm@xxx.yy>
2022-10-06 18:08:07 +0200

    initiation of switching project to typescript

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
-const express = require("express");
-const { join } = require("path");
+import express from "express";
+import { join } from "path";
+import cors from "cors";
+
 const app = express();
-const cors = require("cors");
 
 const port = 3001;
 
 app.use(
   cors({
-    origin: "https://ra.trycodeit.com",
+    origin: "http://localhost:3000",
+    // origin: "https://ra.trycodeit.com",
   })
 );
 app.use(express.json());
@@ -16,7 +18,7 @@ app.use("/api", require("./routes/enemies"));
 app.use("/", require("./routes/enemies"));
 
 app.get("/*", function (req, res) {
-  res.sendFile(join(__dirname, "../public", "index.html"));
+  res.sendFile(join(__dirname, "./public", "index.html"));
 });
 
 app.listen(port, () => {
