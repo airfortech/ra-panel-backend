02230b8977b589d84aff1b7ce9307727c678577b
airfortech <airm@xxx.yy>
2023-02-24 15:05:13 +0100

    migrated to .env config

--- config/config.ts
+++ config/config.ts
@@ -1,12 +1,37 @@
+import * as dotenv from "dotenv";
 import { Config } from "../types/config";
-import { config as configDevelop } from "./config-develop";
-import { config as configProduction } from "./config-production";
+import { UserRole } from "../types/UserRole";
 
-export let config: Config;
+dotenv.config({ path: "./config/.env" });
+const {
+  APP_HOST,
+  APP_PORT,
+  FRONTEND_HOST,
+  DB_URL,
+  SECRET_JWT,
+  CONSIGLIORE_PASSWORD,
+  CAPOREGIME_PASSWORD,
+  SOLDATO_PASSWORD,
+} = process.env;
 
-if (
-  process.env.HEAD_HUNTER_STAGE === "production" ||
-  process.argv[2] === "HEAD_HUNTER_STAGE=production"
-)
-  config = configProduction;
-else config = configDevelop;
+export const config: Config = {
+  app: {
+    host: APP_HOST,
+    port: Number(APP_PORT),
+  },
+  frontend: {
+    host: FRONTEND_HOST,
+  },
+  db: {
+    url: DB_URL,
+  },
+  lang: "pl",
+  secret: {
+    jwt: SECRET_JWT,
+  },
+  users: [
+    { role: UserRole.consigliore, password: CONSIGLIORE_PASSWORD },
+    { role: UserRole.caporegime, password: CAPOREGIME_PASSWORD },
+    { role: UserRole.soldato, password: SOLDATO_PASSWORD },
+  ],
+};
--- utils/languageDetector.ts
+++ utils/languageDetector.ts
@@ -1,11 +1,12 @@
 import { Language } from "../types/Language";
 import { Request } from "../types/Request";
 import { Response, NextFunction } from "express";
+import { config } from "../config/config";
 
 export const languageDetector = () => {
   return (req: Request, res: Response, next: NextFunction) => {
     const language = req.headers["content-language"] as Language;
-    req.lang = language || "en";
+    req.lang = language || config.lang;
     next();
   };
 };
