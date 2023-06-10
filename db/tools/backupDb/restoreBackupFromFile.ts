879d5296effebbfe9a0406126f2176166bbbc56c
airfortech <airm@xxx.yy>
2023-06-10 22:51:18 +0200

    fixed restoreBackupFromFile

--- db/tools/backupDb/restoreBackupFromFile.ts
+++ db/tools/backupDb/restoreBackupFromFile.ts
@@ -6,6 +6,6 @@ const exec = promisify(execCallback);
 
 export const restoreBackupFromFile = async (file: string) => {
   const uri = config.db.url;
-  const backupCommand = `mongorestore --uri=${uri} --archive="./backups/${file}" --drop`;
+  const backupCommand = `mongorestore --uri="${uri}" --archive="./backups/${file}" --drop`;
   await exec(backupCommand);
 };
