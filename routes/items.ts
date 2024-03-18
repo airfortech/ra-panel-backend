51710a9125a487b2ebed1f69b07275a39cbbf370
airfortech <airm@xxx.yy>
2024-03-18 20:17:34 +0100

    refactored adding Items and added rest types of items

--- routes/items.ts
+++ routes/items.ts
@@ -1,9 +1,11 @@
 import { UserRole } from "../types/UserRole";
+import { Request } from "../types/Request";
+import { ItemTypes } from "../types/ItemTypes";
 import { Router } from "express";
 import { auth } from "../utils/auth";
 import { getItems } from "../controllers/items/getItems";
 import { getItem } from "../controllers/items/getItem";
-import { addWeapon } from "../controllers/items/addWeapon";
+import { addItem } from "../controllers/items/addItem";
 
 export const itemsRouter = Router();
 
@@ -28,4 +30,43 @@ itemsRouter
     ),
     getItem
   )
-  .post("/weapon", auth(UserRole.consigliore, UserRole.caporegime), addWeapon);
+  .post(
+    "/weapon",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.weapon)
+  )
+  .post(
+    "/armor",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.armor)
+  )
+  .post(
+    "/shield",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.shield)
+  )
+  .post(
+    "/cloth",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.cloth)
+  )
+  .post(
+    "/jewellery",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.jewellery)
+  )
+  .post(
+    "/stone",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.stone)
+  )
+  .post(
+    "/potion",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.potion)
+  )
+  .post(
+    "/other",
+    auth(UserRole.consigliore, UserRole.caporegime),
+    (req: Request, res) => addItem(req, res, ItemTypes.other)
+  );
